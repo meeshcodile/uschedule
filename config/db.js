b0e8742f4705cebbbc2cdeff393756feae632c8c
misheal <ezekielmisheal4@gmail.com>
2019-10-25 10:21:36 +0100

    update

--- config/db.js
+++ config/db.js
@@ -4,5 +4,5 @@ module.exports = {
 
 
     // =============== database connection to mongodb atlas============
-    MONGOURL:process.env.MONGO
+    MONGOURL:'mongodb://misheal:misheal@cluster0-shard-00-00-zxaqu.mongodb.net:27017,cluster0-shard-00-01-zxaqu.mongodb.net:27017,cluster0-shard-00-02-zxaqu.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
 }
